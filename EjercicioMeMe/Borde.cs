a1eb5a2a0729a06b240b88155627c0136a775d3c
Anthua22 <anthonyemmanuel.ubillus@iesdoctorbalmis.com>
2019-11-01 15:52:59 +0100

    Agregar archivos de proyecto.

---
+++ EjercicioMeMe/Borde.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EjercicioMeMe
+{
+    class Borde : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                if ((bool)value)
+                {
+
+                }
+                return 6;
+            }
+           
+            else return 0;
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
